b5650d0edcaac228b8a410f42fc0a425b844fd84
chengang <chengangyunnan@qq.com>
2018-10-14 20:28:52 +0800

    redefine project folder
    Signed-off-by: chengang <chengangyunnan@qq.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DefaultComponent } from './layout/default/default.component';
+import { FullscreenComponent } from './layout/fullscreen/fullscreen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent,
+    FullscreenComponent
   ],
   imports: [
     BrowserModule
